ea40a2215f124379388d92ace8c73832aca15dd2
Sudip Ghimire <sudipghimire533@gmail.com>
2021-11-07 18:49:58 +0545

    Remove public folder

--- public/js/toggle_menu.js
+++
@@ -1,6 +0,0 @@
-function toggle_menu_visibility() {
-    let target = document.getElementById('i-am-menu-toggle');
-    target.classList.contains('block')? target.classList.remove('block') : target.classList.add('block');
-    target.classList.contains('hidden')? target.classList.remove('hidden') : target.classList.add('hidden');
-}
-
